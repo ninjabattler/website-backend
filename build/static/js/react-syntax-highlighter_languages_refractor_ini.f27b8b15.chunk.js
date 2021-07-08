67f5dfb5f3259d0f614517b26cde956b77f45f94
Jayden Tucker <jaydenrtucker@gmail.com>
2021-07-07 18:07:21 -0700

    Added a new option for ad-auris narrations

--- build/static/js/react-syntax-highlighter_languages_refractor_ini.f27b8b15.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpwebsite=this.webpackJsonpwebsite||[]).push([[60],{225:function(t,s,e){"use strict";function i(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=i,i.displayName="ini",i.aliases=[]}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_refractor_ini.f27b8b15.chunk.js.map
