1d9aa4c60b7b61d7abda601433bf5add7665e98e
Jayden Tucker <jaydenrtucker@gmail.com>
2021-08-30 23:27:21 -0700

    Updated mobile view

---
+++ build/static/js/react-syntax-highlighter_languages_refractor_ini.024c40df.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpwebsite=this.webpackJsonpwebsite||[]).push([[60],{227:function(t,s,e){"use strict";function i(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=i,i.displayName="ini",i.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_ini.024c40df.chunk.js.map
