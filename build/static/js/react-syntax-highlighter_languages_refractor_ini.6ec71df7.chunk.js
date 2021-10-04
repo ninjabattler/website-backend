5f5fedaca384320f2191060a6a0614e812ddfea3
Jayden Tucker <jaydenrtucker@gmail.com>
2021-10-03 20:49:20 -0700

    Updated 480px width view for the review page, and disabled the text particles temporarily

--- build/static/js/react-syntax-highlighter_languages_refractor_ini.6ec71df7.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpwebsite=this.webpackJsonpwebsite||[]).push([[60],{226:function(t,s,e){"use strict";function i(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=i,i.displayName="ini",i.aliases=[]}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_refractor_ini.6ec71df7.chunk.js.map
